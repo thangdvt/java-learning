9d17c68269f91f09511d8fca6780d04fb0542bed
dang thang <thangdv@epayjsc.com>
2024-01-22 13:29:36 +0700

    update

--- Lession-04/src/App.java
+++ Lession-04/src/App.java
@@ -11,6 +11,14 @@ public class App {
         System.out.println("nhap vao ma lop hoc");
         int maLopHoc = sc.nextInt();
         System.out.println("-------------");
-
+        System.out.println("ho va ten " + hoTen + "co " +maSinhVien +"hoc o lop co ma"+ maLopHoc);
+        System.out.println("nhap vao ten");
+        String abc = sc.nextLine();
+        System.out.println("ma sinh vien");
+        long xzz = sc.nextLong();
+        System.out.println("nhap vao ma lop hoc");
+        int cxcx = sc.nextInt();
+        System.out.println("-------------");
+        System.out.println("ho va ten " + hoTen + "co " +maSinhVien +"hoc o lop co ma"+ maLopHoc);
     }
 }
