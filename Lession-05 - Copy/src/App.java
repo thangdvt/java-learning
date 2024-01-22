105bf20539629befdfc381b9736a069b47d5667b
dang thang <thangdv@epayjsc.com>
2024-01-22 14:52:02 +0700

    THANG38TES-18
    test

---
+++ Lession-05 - Copy/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args){
+        int a = 100;
+        int b = 200;
+        System.out.println(a);
+        System.out.println(b);
+        float c = a;
+        float d = b;
+        System.out.println(c);
+        System.out.println(d);
+        float e = 3.5f;
+        float f = 10.2f;
+        System.out.println(e);
+        System.out.println(f);
+        int g = (int)e;
+        int h = (int)f;
+        System.out.println(g);
+        System.out.println(h);
+        int x = new Integer(32);
+        System.out.println(x);       
+    }
+}
