808108e34a6f971d8fa52b855763c5990b1c8100
dang thang <thangdv@epayjsc.com>
2024-01-22 14:23:18 +0700

    first commit

---
+++ Lession-06 - Copy/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int a = 5;
+        boolean b = false;
+        System.out.println("so a = " + (-a));
+        System.out.println("so a = " + (+a));
+        System.out.println("so b = " + (!b));
+        System.out.println("so ++a = " + (++a));
+        System.out.println("so a++ = " + (a++));
+        System.out.println("so a-- = " + (a--));
+    }
+}
+ 
